db95a9b082d635924dcb47ee9c3946d52b2b3634
17jwong <157659293+17jwong@users.noreply.github.com>
2024-05-05 15:39:55 -0400

    Update interface.py

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -84,7 +84,6 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3527 * CV.LB_TO_KG
       ret.wheelbase = 2.87
       ret.steerRatio = 16.5
-      ret.steerActuatorDelay = 0.36
     elif candidate in (CAR.CX_60, CAR.CX_80, CAR.CX_70, CAR.CX_90):
       ret.mass = 4217 * CV.LB_TO_KG
       ret.wheelbase = 3.1
