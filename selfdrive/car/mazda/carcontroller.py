facebb4d7daf0f1e373d08ee47843306042fdf73
17jwong <157659293+17jwong@users.noreply.github.com>
2024-10-03 19:36:43 -0400

    Update carcontroller.py

--- selfdrive/car/mazda/carcontroller.py
+++ selfdrive/car/mazda/carcontroller.py
@@ -29,7 +29,7 @@ class CarController(CarControllerBase):
     self.filtered_acc_last = 0
     self.params = Params()
     self.params_memory = Params("/dev/shm/params")
-    self.params_memory.put("CEFramesCounter", 0)
+    self.params_memory.put_int("CEFramesCounter", 0)
 
 
   def update(self, CC, CS, now_nanos, frogpilot_toggles):
